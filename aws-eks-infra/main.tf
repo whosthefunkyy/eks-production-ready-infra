2d93470f7c2008009be2fcb91ff1451b7f2f2c09
artem <tenet@MacBook-Air-artem.local>
2026-06-20 23:08:19 +0700

    fix

--- aws-eks-infra/main.tf
+++ aws-eks-infra/main.tf
@@ -89,85 +89,85 @@ access_entries = {
   }
 }
 ### Policy
-resource "aws_iam_policy" "aws_load_balancer_controller" {
-  name = "AWSLoadBalancerControllerIAMPolicy"
+# resource "aws_iam_policy" "aws_load_balancer_controller" {
+#   name = "AWSLoadBalancerControllerIAMPolicy"
 
-  policy = file("${path.module}/iam/aws-load-balancer-controller-policy.json")
-}
+#   policy = file("${path.module}/iam/aws-load-balancer-controller-policy.json")
+# }
 
 #### Role for LB Controller
-resource "aws_iam_role" "aws_load_balancer_controller" {
-  name = "AmazonEKSLoadBalancerControllerRole"
+# resource "aws_iam_role" "aws_load_balancer_controller" {
+#   name = "AmazonEKSLoadBalancerControllerRole"
 
- assume_role_policy = jsonencode({
-  Version = "2012-10-17"
+#  assume_role_policy = jsonencode({
+#   Version = "2012-10-17"
 
-  Statement = [
-    {
-      Effect = "Allow"
+#   Statement = [
+#     {
+#       Effect = "Allow"
 
-      Principal = {
-        Federated = module.eks.oidc_provider_arn
-      }
+#       Principal = {
+#         Federated = module.eks.oidc_provider_arn
+#       }
 
-      Action = "sts:AssumeRoleWithWebIdentity"
+#       Action = "sts:AssumeRoleWithWebIdentity"
 
-      Condition = {
-        StringEquals = {
-          "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+#       Condition = {
+#         StringEquals = {
+#           "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
 
-          "${module.eks.oidc_provider}:aud" = "sts.amazonaws.com"
-        }
-      }
-    }
-  ]
-})
-}
+#           "${module.eks.oidc_provider}:aud" = "sts.amazonaws.com"
+#         }
+#       }
+#     }
+#   ]
+# })
+# }
 
 ### Role + Policy
-resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller" {
-  role       = aws_iam_role.aws_load_balancer_controller.name
-  policy_arn = aws_iam_policy.aws_load_balancer_controller.arn
-}
+# resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller" {
+#   role       = aws_iam_role.aws_load_balancer_controller.name
+#   policy_arn = aws_iam_policy.aws_load_balancer_controller.arn
+# }
 
-resource "kubernetes_service_account" "aws_load_balancer_controller" {
-  metadata {
-    name      = "aws-load-balancer-controller"
-    namespace = "kube-system"
+# resource "kubernetes_service_account" "aws_load_balancer_controller" {
+#   metadata {
+#     name      = "aws-load-balancer-controller"
+#     namespace = "kube-system"
  
 
-  annotations = {
-    "eks.amazonaws.com/role-arn" = aws_iam_role.aws_load_balancer_controller.arn
-  }
- }
-   depends_on = [module.eks] 
+#   annotations = {
+#     "eks.amazonaws.com/role-arn" = aws_iam_role.aws_load_balancer_controller.arn
+#   }
+#  }
+#    depends_on = [module.eks] 
   
-}
+# }
 
-resource "helm_release" "aws_load_balancer_controller" {
-  name       = "aws-load-balancer-controller"
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-  namespace  = "kube-system"
+# resource "helm_release" "aws_load_balancer_controller" {
+#   name       = "aws-load-balancer-controller"
+#   repository = "https://aws.github.io/eks-charts"
+#   chart      = "aws-load-balancer-controller"
+#   namespace  = "kube-system"
   
-  depends_on = [
-    module.eks,
-    kubernetes_service_account.aws_load_balancer_controller
-  ]
+#   depends_on = [
+#     module.eks,
+#     kubernetes_service_account.aws_load_balancer_controller
+#   ]
 
 
-  set {
-    name  = "clusterName"
-    value = module.eks.cluster_name
-  }
+#   set {
+#     name  = "clusterName"
+#     value = module.eks.cluster_name
+#   }
 
-  set {
-    name  = "serviceAccount.create"
-    value = "false"
-  }
+#   set {
+#     name  = "serviceAccount.create"
+#     value = "false"
+#   }
 
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
-}
+#   set {
+#     name  = "serviceAccount.name"
+#     value = "aws-load-balancer-controller"
+#   }
+# }
