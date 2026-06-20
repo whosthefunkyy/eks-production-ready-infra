7ae9f6b666cfe7c4fd7c716d619ed27fd2f6ee7b
artem <tenet@MacBook-Air-artem.local>
2026-06-20 22:05:14 +0700

    fix

--- aws-eks-infra/providers.tf
+++ aws-eks-infra/providers.tf
@@ -22,22 +22,26 @@ provider "aws" {
 }
 
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.aws_region]
+  }
+}
 
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.aws_region]
+    }
   }
-}
-
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
 }
